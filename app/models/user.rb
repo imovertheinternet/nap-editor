c4cb7699d90f7904c25a8f19bd34ffa7ebbbce79
conrad grant <conrad@macbookbro.(none)>
2013-06-17 12:46:25 -0500

    updating user.rb file

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ActiveRecord::Base
+has_and_belongs_to_many :posts
+
+#  has_many :posts
+
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+  # attr_accessible :title, :body
+end
