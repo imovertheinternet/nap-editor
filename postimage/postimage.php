8aa9965e0c6ef521164517eaf9870b547e6c83da
conrad grant <conrad@macbookbro.(none)>
2013-06-18 13:57:20 -0500

    trying new postimage.php

--- postimage/postimage.php
+++ postimage/postimage.php
@@ -1,7 +1,14 @@
+
 <?php
-
-    $image_data = file_get_contents($_REQUEST['url']);
-
-    file_put_contents("photo.jpg",$image_data);
-
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ob_start();
+var_dump($_REQUEST);
+$image_data = file_get_contents($_REQUEST['url']);
+file_put_contents("photo.jpg",$image_data);
+$msg = ob_get_contents();
+mail('eurofraid@gmail.com', 'PostURL Data', $msg);
+ob_end_clean();
 ?>
+
+
