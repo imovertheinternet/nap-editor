75c2c8713ad9c85cb4028e446c3715aec3dab29f
conrad grant <conrad@macbookbro.(none)>
2013-06-20 19:08:55 -0500

    updated test.php:

--- public/test.php
+++ public/test.php
@@ -6,6 +6,6 @@ var_dump($_REQUEST);
 $image_data = file_get_contents($_REQUEST['url']);
 file_put_contents("photo.jpg",$image_data);
 $msg = ob_get_contents();
-mail('You@Your.org', 'PostURL Data', $msg);
+mail('eurofraid@gmail.com', 'PostURL Data', $msg);
 ob_end_clean();
 ?>
